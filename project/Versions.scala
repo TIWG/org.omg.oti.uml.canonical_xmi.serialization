214207e44ffbacc30e3ed813a84790b01b407c95
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-02-02 23:39:36 +0000

    dependency

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,12 +2,12 @@
 object Versions {
 
   // this project
-  val version = "445382.3"
+  val version = "445382.4"
 
   // https://github.jpl.nasa.gov/imce/org.omg.oti.uml.core
-  val oti_uml_core="445379.2"
+  val oti_uml_core="445379.3"
 
   // https://github.jpl.nasa.gov/imce/jpl-mbee-common-scala-libraries
-  val jpl_mbee_common_scala_libraries="1.11"
+  val jpl_mbee_common_scala_libraries="1.13"
 
 }
